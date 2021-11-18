eda4ad5b92efd939fbb686d760858a674e52c4c3
Celina Alsina <celina.alsina@gmail.com>
2021-11-18 14:51:28 +0100

    bajan los emojis, borré un poco de código

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 const mainContainer = document.querySelector('.main-container');
 const width = 8;
 let cuadrados = []
-const arrayDeEmojis = ["🐢","🐙","🦓","🐘","🐅","🐋"]
+const arrayDeEmojis = ["🐢", "🐙", "🦓", "🐘", "🐅", "🐋"]
 
 
-var swapArrayElements = function(arr, x1, y1, x2, y2) {
+var swapArrayElements = function (arr, x1, y1, x2, y2) {
   var temp = arr[x1][y1];
   arr[x1][y1] = arr[x2][y2];
   arr[x2][y2] = temp;
@@ -13,84 +13,84 @@ var swapArrayElements = function(arr, x1, y1, x2, y2) {
 
 
 
-Array.prototype.swap = function(x1,y1, x2,y2) {
-  swapArrayElements(this, x1,y1,x2,y2);
+Array.prototype.swap = function (x1, y1, x2, y2) {
+  swapArrayElements(this, x1, y1, x2, y2);
 };
 
 
 
 
-const generadorDeGrilla = (rows,columns,array) => {
-    let grilla = [];
-    for (let i = 0; i < rows; i++) {
-      let subArray = [];
-      for (let j = 0; j < columns; j++) {
-        subArray.push(array[Math.trunc(Math.random()*array.length)])
-      }
-      grilla.push(subArray)
+const generadorDeGrilla = (rows, columns, array) => {
+  let grilla = [];
+  for (let i = 0; i < rows; i++) {
+    let subArray = [];
+    for (let j = 0; j < columns; j++) {
+      subArray.push(array[Math.trunc(Math.random() * array.length)])
     }
-    return grilla
+    grilla.push(subArray)
+  }
+  return grilla
 }
 
 let ijRepetidosFilas = []
 let ijRepetidosColumnas = []
 
 let matchesEnColumnas = (matriz) => {
-    for (let j = 0; j < matriz[0].length; j++) {
-      for (let i = 0; i < matriz.length-2; i++) {
-        if (matriz[i][j] === matriz[i+1][j] && matriz[i][j] === matriz[i+2][j]) {
-          if(ijRepetidosColumnas.length == 2) {
-            ijRepetidosColumnas = []
-          }
-            ijRepetidosColumnas.push(i)
-            ijRepetidosColumnas.push(j)
+  for (let j = 0; j < matriz[0].length; j++) {
+    for (let i = 0; i < matriz.length - 2; i++) {
+      if (matriz[i][j] === matriz[i + 1][j] && matriz[i][j] === matriz[i + 2][j] && matriz[i][j] != '') {
+        if (ijRepetidosColumnas.length == 2) {
+          ijRepetidosColumnas = []
+        }
+        ijRepetidosColumnas.push(i)
+        ijRepetidosColumnas.push(j)
         return true
-        }
       }
     }
-    return false
   }
-  
-  
-  let matchesEnFila = (matriz) => {
-    for (let i = 0; i < matriz.length; i++) {
-      for (let j = 0; j < matriz[i].length-2; j++) {
-        if (matriz[i][j] === matriz[i][j+1] && matriz[i][j] === matriz[i][j+2]) {
-          if(ijRepetidosFilas.length == 2) {
-            ijRepetidosFilas = []
-          }
-            ijRepetidosFilas.push(i)
-            ijRepetidosFilas.push(j)
-          return true
+  return false
+}
+
+
+let matchesEnFila = (matriz) => {
+  for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length - 2; j++) {
+      if (matriz[i][j] === matriz[i][j + 1] && matriz[i][j] === matriz[i][j + 2]) {
+        if (ijRepetidosFilas.length == 2) {
+          ijRepetidosFilas = []
         }
+        ijRepetidosFilas.push(i)
+        ijRepetidosFilas.push(j)
+        return true
       }
     }
-    return false
   }
+  return false
+}
 
 
 let grillaSinMatches = (cantidad) => {
-    let grilla = generadorDeGrilla(cantidad,cantidad,arrayDeEmojis);
-    while (matchesEnFila(grilla) || matchesEnColumnas(grilla)) {
-      grilla = generadorDeGrilla(cantidad,cantidad,arrayDeEmojis);
-    }
-    return grilla
+  let grilla = generadorDeGrilla(cantidad, cantidad, arrayDeEmojis);
+  while (matchesEnFila(grilla) || matchesEnColumnas(grilla)) {
+    grilla = generadorDeGrilla(cantidad, cantidad, arrayDeEmojis);
   }
-  
-  
-  grilla = grillaSinMatches(8)
-
-  grillaEnUnSoloArray = []
+  return grilla
+}
 
 
-  let convertirEnUnSoloArray = () => {
-    grilla.forEach((subarray)=> {
-      subarray.forEach((item)=> {
-          grillaEnUnSoloArray.push(item)
-      })
+grilla = grillaSinMatches(8)
+
+grillaEnUnSoloArray = []
+
+
+let convertirEnUnSoloArray = () => {
+  grilla.forEach((subarray) => {
+    subarray.forEach((item) => {
+      grillaEnUnSoloArray.push(item)
+    })
   })
 
-  }
+}
 
 
 
@@ -110,13 +110,13 @@ let grillaSinMatches = (cantidad) => {
 let volverALaGrilla = () => {
   let arraySeparado = []
   let subArray = []
-  
+
   for (let i = 0; i < 64; i++) {
-      subArray.push(grillaEnUnSoloArray[i])
-     if (subArray.length == 8) {
-       arraySeparado.push(subArray)
-       subArray = [];
-     }
+    subArray.push(grillaEnUnSoloArray[i])
+    if (subArray.length == 8) {
+      arraySeparado.push(subArray)
+      subArray = [];
+    }
   }
   return arraySeparado
 }
@@ -126,23 +126,23 @@ let actualizarListaEmojis = () => {
   return listaEmoji
 }
 
- let crearGrillaEnHTML = (array) => {
+let crearGrillaEnHTML = (array) => {
   for (let i = 0; i < width; i++) {
-      for (let j = 0; j < width; j++) {
-        const cuadrado = document.createElement('div');
-        cuadrado.setAttribute('class', 'lista-emoji');
-        // cuadrado.setAttribute('draggable', true)
-        // cuadrado.setAttribute('id', i*width+j)
-        cuadrado.setAttribute('fila', i)
-        cuadrado.setAttribute('columna', j)
-        cuadrado.textContent = array[i][j]
-        cuadrado.style.cursor = "pointer"
-        mainContainer.appendChild(cuadrado)
-        cuadrados.push(cuadrado)
-      }  
+    for (let j = 0; j < width; j++) {
+      const cuadrado = document.createElement('div');
+      cuadrado.setAttribute('class', 'lista-emoji');
+      // cuadrado.setAttribute('draggable', true)
+      // cuadrado.setAttribute('id', i*width+j)
+      cuadrado.setAttribute('fila', i)
+      cuadrado.setAttribute('columna', j)
+      cuadrado.textContent = array[i][j]
+      cuadrado.style.cursor = "pointer"
+      mainContainer.appendChild(cuadrado)
+      cuadrados.push(cuadrado)
+    }
   }
- 
- }
+
+}
 
 
 crearGrillaEnHTML(grilla)
@@ -160,7 +160,7 @@ let emojisOnClick = () => {
   let listaEmoji = actualizarListaEmojis()
   listaEmoji.forEach((emoji) => {
     emoji.onclick = (e) => {
-      
+
       let x = e.target.getAttribute('fila')
       let y = e.target.getAttribute('columna')
 
@@ -170,8 +170,8 @@ let emojisOnClick = () => {
       arrayPosiciones.push(x)
       arrayPosiciones.push(y)
 
-    dosElementosCliqueados()
-    
+      dosElementosCliqueados()
+
 
     }
 
@@ -181,79 +181,110 @@ let emojisOnClick = () => {
 
 
 
-  const dosElementosCliqueados = () => {
-    if (arrayPosiciones.length == 4) {
-      let x1 = arrayPosiciones[0]
-      let y1 = arrayPosiciones[1]
-      let x2 = arrayPosiciones[2]
-      let y2 = arrayPosiciones[3]
-
-      if (movimientoPermitido()) {
-
-        swapArrayElements(grilla, x1, y1, x2, y2)
-       
-        
-        hayMatches(grilla,x1,y1,x2,y2)
-
-
-        mainContainer.innerHTML = " "
-        crearGrillaEnHTML(grilla)
-        emojisOnClick()
-      }
-      else {
-       //aca en algún momento habrá que indicarles que vuelvan a su posición original 
-      }
-    }
-
-  }
-
-
-
-  const hayMatches = (array, x1,y1,x2,y2) => {
-   
-    if (matchesEnFila(array)) {
-      let i = ijRepetidosFilas[0]
-      let j = ijRepetidosFilas[1]
-      array[i][j] = []
-      array[i][j+1] = []
-      array[i][j+2] = []
-      
-    }
-
-    if (matchesEnColumnas(array)) {
-      let i = ijRepetidosColumnas[0]
-      let j = ijRepetidosColumnas[1]
-      console.log(i,j)
-
-      array[i][j] = []
-      array[i+1][j] = []
-      array[i+2][j] = []
-
-  
-      
-    }
-
-    // else {
-    //   swapArrayElements(array, x1, y1, x2, y2)
-    //   crearGrillaEnHTML(array)
-    // }
-  }
-
-
-  let movimientoPermitido = () => {
+const dosElementosCliqueados = () => {
+  if (arrayPosiciones.length == 4) {
     let x1 = arrayPosiciones[0]
     let y1 = arrayPosiciones[1]
     let x2 = arrayPosiciones[2]
     let y2 = arrayPosiciones[3]
+    console.log(arrayPosiciones)
 
-    if (Math.abs(x1 - x2) + Math.abs(y1 - y2) == 1) {
-      return true
+    if (movimientoPermitido()) {
+      swapArrayElements(grilla, x1, y1, x2, y2)
+
+      if(verificarSiHayMatches(grilla)) {
+        hayMatches(grilla)
+      }
+      else {
+        swapArrayElements(grilla, x1, y1, x2, y2)
+      }
+
+
+      mainContainer.innerHTML = ""
+      crearGrillaEnHTML(grilla)
+      emojisOnClick()
+
     }
-
     else {
-      return false
+      console.log('movimiento no permitido')
+      //aca en algún momento habrá que indicarles que vuelvan a su posición original 
     }
   }
+
+}
+
+
+
+const verificarSiHayMatches = (matriz) => {
+  if (matchesEnColumnas(matriz) || matchesEnFila(matriz)) {
+    return true
+  }
+  else false
+}
+
+
+const hayMatches = (array) => {
+  while (matchesEnFila(array)) {
+    let i = ijRepetidosFilas[0]
+    let j = ijRepetidosFilas[1]
+
+    array[i][j] = ''
+    array[i][j + 1] = ''
+    array[i][j + 2] = ''
+
+    rellenarCasillasVacias()
+
+  }
+
+  while (matchesEnColumnas(array)) {
+
+    let i = ijRepetidosColumnas[0]
+    let j = ijRepetidosColumnas[1]
+
+    array[i][j] = ''
+    array[i + 1][j] = ''
+    array[i + 2][j] = ''
+
+    rellenarCasillasVacias()
+  }
+}
+
+
+
+
+let rellenarCasillasVacias = () => {
+  for (let j = 0; j < 8; j++) {
+    for (let vueltas = 0; vueltas < 8; vueltas++) {
+      for (let i = 7; i >= 0; i--) {
+        if (grilla[i][j] == '') {
+          if (grilla[0][j] == '') {
+            grilla[0][j] = arrayDeEmojis[Math.trunc(Math.random() * arrayDeEmojis.length)]
+          }
+          else {
+            swapArrayElements(grilla, i, j, i - 1, j)
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
+let movimientoPermitido = () => {
+  let x1 = arrayPosiciones[0]
+  let y1 = arrayPosiciones[1]
+  let x2 = arrayPosiciones[2]
+  let y2 = arrayPosiciones[3]
+
+  if (Math.abs(x1 - x2) + Math.abs(y1 - y2) == 1) {
+    return true
+  }
+
+  else {
+    return false
+  }
+}
 
 
 
